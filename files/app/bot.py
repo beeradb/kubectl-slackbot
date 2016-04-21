6a20bb17c3e3a91e7b03c9f6389368bb99cecf52
Brad Bowman <beeradb@gmail.com>
2016-04-21 11:42:07 -0600

    [master] Add basic kube command handling

--- files/app/bot.py
+++ files/app/bot.py
@@ -1,7 +1,7 @@
 from slackbot.bot import Bot
 from slackbot.bot import listen_to
-import re
-
+import subprocess
+import os
 
 def main():
     bot = Bot()
@@ -9,9 +9,44 @@ def main():
 
 
 @listen_to('kubectl (.*)')
-def kubectl(message, something):
-    message.reply('Here is {}'.format(something))
+def kubectl(message, kube_command):
+    result = kubectl(kube_command)
+    message.reply(result)
 
+
+def kubectl(command):
+    """
+    Run a kubect command and attempt to parse the results as json.
+    :param command: the command to run
+    :param json: Whether or not to output results in json.
+    :param env: The environment to use.
+    :return: dict if parsed as json, raw output if not. Raises
+             subprocess.calledProcessError on command fail.
+    """
+    env = os.environ.copy()
+
+    with open('/var/run/secrets/kubernetes.io/serviceaccount/token') as sa_token:
+        token = sa_token.read()
+
+    cmd = "/bin/kubectl {cmd} --token={token} --server={url} --insecure-skip-tls-verify=true".format(cmd=command, token=token, url=kube_api_url())
+
+
+    # This will throw subprocess.CalledProcessError if the command fails.
+    # Code that calls run_command should be in a try/except to handle the failure case.
+    result = subprocess.check_output(shlex.split(cmd), env=env, stderr=subprocess.STDOUT)
+
+    return result
+
+
+def kube_api_url():
+    """
+    Returns the kubernetes API url
+    """
+    return "https://{hostname}:{port}".format(
+        hostname=os.environ['KUBERNETES_SERVICE_HOST'],
+        port=os.environ['KUBERNETES_SERVICE_PORT']
+    )
 
 if __name__ == "__main__":
     main()
+
